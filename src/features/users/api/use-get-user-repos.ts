b834fc5689ad66c2102c86b5f9de1a5bb213141b
ecaleb97 <valley_rob.0c@icloud.com>
2024-11-25 16:37:25 +0100

    Enhance useGetUserRepos hook to accept repoCount parameter and return sliced data

--- src/features/users/api/use-get-user-repos.ts
+++ src/features/users/api/use-get-user-repos.ts
@@ -1,9 +1,9 @@
 import type { GithubReposResponse } from "@/types/types";
 import { useQuery } from "@tanstack/react-query";
 
-export function useGetUserRepos(search?: string) {
+export function useGetUserRepos(search?: string, repoCount?: number) {
 	const query = useQuery({
-		queryKey: ["getUserRepos", search],
+		queryKey: ["getUserRepos", search, repoCount],
 		queryFn: async () => {
 			const response = await fetch(
 				`https://api.github.com/users/${search}/repos`,
@@ -13,7 +13,8 @@ export function useGetUserRepos(search?: string) {
 				throw new Error(`Error searching user: ${response.status}`);
 
 			const data = (await response.json()) as GithubReposResponse[];
-			return data;
+			const slicedData = data.slice(0, repoCount);
+			return { data, slicedData };
 		},
 	});
 
